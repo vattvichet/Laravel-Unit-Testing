a46bcd7fd727cbe585c4da695b50e133b0f7240e
vichet vatt <vichetvattedu@gmail.com>
2024-04-30 20:11:23 +0700

    Tested incomplete request

--- app/Http/Controllers/TodoListController.php
+++ app/Http/Controllers/TodoListController.php
@@ -8,9 +8,20 @@ use Illuminate\Support\Facades\Http;
 
 class TodoListController extends Controller
 {
-    public function getTodoList()
+    public function index()
     {
         $response = Http::get('https://dummyjson.com/todos?limit=3');
+        $data = $response->json();
+        return response()->json($data, $response->status());
+    }
+
+    public function store(Request $request)
+    {
+        $response = Http::post('https://dummyjson.com/todos/add', [
+            'todo' => $request->todo,
+            'completed' => $request->completed,
+            'userId' => $request->userId,
+        ]);
         $data = $response->json();
         return response()->json($data, $response->status());
     }
--- tests/Feature/TodoListTest.php
+++ tests/Feature/TodoListTest.php
@@ -13,7 +13,7 @@ class TodoListTest extends TestCase
      */
     public function test_getTodoList(): void
     {
-        $response = $this->getJson('api/todo-list');
+        $response = $this->get('api/todo-list');
         $response->assertStatus(200);
         // Make sure that the response match with mocked structure
         $response->assertJsonStructure([
@@ -28,5 +28,34 @@ class TodoListTest extends TestCase
         // Decode the JSON response into an array
         $responseData = $response->json();
         $this->assertEquals(150, $responseData['total']);
+    }
+
+    public function test_postTodo()
+    {
+        $mock_body = [
+            'userId' => 1,
+            'completed' => false,
+            "todo" => "Go back home"
+        ];
+        $response = $this->post('api/todo-list', $mock_body);
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            "id",
+            "todo",
+            "completed",
+            "userId",
+        ]);
+
+        // Test case 3: Testing with incorrect data type
+        $incorrectData = [
+            'userId' => 'not an integer', // userId should be an integer
+            'completed' => false,
+            'todo' => 'Do something'
+        ];
+        $response = $this->postJson('/api/todo-list', $incorrectData);
+        $response->assertStatus(400);
+
+        // Test case 4: Test database state
+        // $this->assertDatabaseHas('todos', ['todo' => 'Go back home']);
     }
 }
